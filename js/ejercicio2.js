1f7f16a067f3f4dbece3dc916b6c516b2b9faa62
Mike <maik_kb@live.com.mx>
2018-07-24 16:09:03 -0500

    leccion12

---
+++ js/ejercicio2.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/* 
+Utilizando un bucle mostrar la suma y l amedia de los numeros intriducidos
+hasta intriducir un numero negativo y ahi mostrar el resultado
+*/
+
+
+var suma = 0;
+var contador = 0;
+
+do{
+	var numero = parseInt(prompt('Introduce numeros hasta que metas uno negativo', 0));
+
+	if(isNaN(numero)){
+		numero = 0;
+	}else if(numero >=0){
+		suma += numero;
+		contador++;
+	}
+
+	console.log('la suma '+contador+' es: '+suma);
+}while(numero >= 0)
